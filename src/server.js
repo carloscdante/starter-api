7f2666e9cee8a1042496af250ac55892ba098971
Carlos Dante <ceh@certi.org.br>
2022-07-21 14:36:29 -0300

    chore: added pino logger

--- src/server.js
+++ src/server.js
@@ -1,6 +1,17 @@
+import pino from 'pino-http';
+
+const logger = pino().logger;
 const { app } = require('./app');
 const { SERVER_PORT } = require('./environment');
 
 const PORT = SERVER_PORT || 5000;
+const env = process.env.ENVIRONMENT;
 
-app.listen(PORT, () => console.log(`server listening on port ${PORT}`));
+app.use(pino());
+
+app.listen(PORT, () => {
+  logger.info('=================================');
+  logger.info(`======= ENV: ${env} =======`);
+  logger.info(`🚀 App listening on the port ${PORT}`);
+  logger.info('=================================');
+});
